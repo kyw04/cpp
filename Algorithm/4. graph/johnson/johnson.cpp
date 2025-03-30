b826f9f0fab0b439baf467fb2006d5db9940a23e
kyw04 <85551017+kyw04@users.noreply.github.com>
2025-03-30 13:18:19 +0000

    fix: johnson algorithm

--- Algorithm/4. graph/johnson/johnson.cpp
+++ Algorithm/4. graph/johnson/johnson.cpp
@@ -4,36 +4,128 @@
     이 알고리즘은 음수 가중치를 갖는 그래프에서도 올바른 결과을 반환합니다.
 */
 #include "../Graph.hpp"
-#include "../dijkstra/dijkstra.hpp"
-#include "../bellman ford/bellman_ford.hpp"
+#include <limits>
+
+template <typename T>
+const T UNKNOWN = std::numeric_limits<T>::max();
+
+
+template <typename T>
+bool HasNegativeCycle(std::vector<Edge<T>> edges, std::vector<T> distance)
+{
+    for (auto& e : edges)
+    {
+        if (distance[e.src] == UNKNOWN<T>) { continue; }
+
+        if (distance[e.src] + e.weight < distance[e.dst])
+            return true;
+    }
+
+    return false;
+}
+
+template <typename T>
+std::vector<T> BellmanFord(std::vector<Edge<T>> edges, std::size_t v)
+{
+    std::vector<T> distance(v + 1, UNKNOWN<T>);
+    unsigned s = v;
+    for (std::size_t i = 1; i < v; i++)
+    {
+        edges.push_back(Edge<T>{ s, i, 0 });
+    }
+    distance[s] = 0;
+
+    for (std::size_t i = 1; i < v; i++)
+    {
+        for (auto& e : edges)
+        {
+            if (distance[e.src] == UNKNOWN<T>) { continue; }
+
+            distance[e.dst] = std::min(distance[e.dst], distance[e.src] + e.weight);
+        }
+    }
+    
+    if (HasNegativeCycle(edges, distance))
+    {
+        std::cout << "[음수 가중치 발견]" << std::endl;
+        return { };
+    }
+
+    return distance;
+}
+
+template <typename T>
+std::size_t GetMinDistance(std::vector<T> distance, std::vector<bool> visited)
+{
+    T min_distance = UNKNOWN<T>;
+    std::size_t min_index = 0;
+
+    for (std::size_t i = 0; i < distance.size(); i++)
+    {
+        if (!visited[i] && distance[i] <= min_distance)
+        {
+            min_distance = distance[i];
+            min_index = i;
+        }
+    }
+
+    return min_index;
+}
+
+template <typename T>
+std::vector<T> Dijkstra(Graph<T> g, unsigned src)
+{
+    std::size_t v = g.GetVertices();
+    std::vector<T> distance(v, UNKNOWN<T>);
+    std::vector<bool> visited(v, false);
+    distance[src] = 0;
+
+    for (std::size_t i = 0; i < v - 1; i++)
+    {
+        std::size_t current = GetMinDistance(distance, visited);
+        visited[current] = true;
+
+        for (auto& e : g.GetEdges())
+        {
+            if (e.src != current || visited[e.dst]) { continue; }
+
+            if (distance[current] != UNKNOWN<T>)
+            {
+                distance[e.dst] = std::min(distance[e.dst], distance[current] + e.weight);
+            }
+        }
+    }
+
+    return distance;
+}
 
 template <typename T>
 std::vector<T> Johnson(Graph<T> g)
 {
-    std::size_t src = 1;
-    std::vector<T> h = BellmanFord(g, src);
+    std::size_t v = g.GetVertices();
+    std::vector<T> h = BellmanFord(g.GetEdges(), v);
 
     if (h.empty())
         return { };
 
+    Graph<T> plus_g(v);
     for (auto& e : g.GetEdges())
     {
-        e.weight += h[e.src] - h[e.dst];
+        plus_g.AddEdge(Edge<T>{ e.src, e.dst, e.weight + h[e.src] - h[e.dst] });
     }
 
-    std::size_t v = g.GetVertices();
-    std::vector<Graph<T>> shortest(v);
-    for (std::size_t i = 0; i < v; i++)
+    std::vector<std::vector<T>> shortest(v);
+    for (std::size_t i = 1; i < v; i++)
     {
-        shortest[i] = Dijkstra(g, 1, i + 1);
+        shortest[i] = Dijkstra(plus_g, i);
     }
 
-    for (std::size_t i = 0; i < v; i++)
+    for (std::size_t i = 1; i < v; i++)
     {
         std::cout << i << ":\n";
-        for (std::size_t j = 0; j < v; i++)
+        for (std::size_t j = 1; j < v; j++)
         {
-            if (shortest[i][j] != UNKNOWN)
+            if (shortest[i][j] != UNKNOWN<T>)
             {
                 shortest[i][j] += h[j] - h[i];
 
